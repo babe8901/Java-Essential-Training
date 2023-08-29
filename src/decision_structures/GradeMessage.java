c3e43894641aca02b830d7cdcab6249ea7f29098
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-29 18:11:41 +0530

    GradeMessage(switch statement) completed

---
+++ src/decision_structures/GradeMessage.java
@@ -0,0 +1,36 @@
+package decision_structures;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+        String message;
+        switch(grade) {
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great Job!";
+                break;
+            case "C":
+                message = "Good Job!";
+                break;
+            case "D":
+                message = "You need to work a bit harder";
+                break;
+            case "F":
+                message = "Uh Oh!";
+                break;
+            default:
+                message = "Error. Invalid grade!";
+                break;
+        }
+
+        System.out.println(message);
+    }
+}
