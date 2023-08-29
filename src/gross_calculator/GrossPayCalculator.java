b5a4bcf5fbb6ed0f85606af6edd929e77b35a4bb
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-29 14:06:10 +0530

    Simple Gross Pay Calculator created

---
+++ src/gross_calculator/GrossPayCalculator.java
@@ -0,0 +1,18 @@
+package gross_calculator;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+        payRate = scanner.nextDouble();
+        scanner.close();
+        double grossPay = hours * payRate;
+        System.out.println("Gross Pay: " + grossPay);
+    }
+}
