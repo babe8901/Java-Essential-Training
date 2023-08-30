92bb13e27f093df3c0e5bc47218be6b75fed8de4
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-31 01:04:07 +0530

    QualifyForLoan(Logical Operators) completed

---
+++ src/operators/QualifyForLoan.java
@@ -0,0 +1,22 @@
+package operators;
+
+import java.util.Scanner;
+
+public class QualifyForLoan {
+    public static void main(String[] args) {
+        double salary, requiredSalary = 30000;
+        int years, requiredYearsEmployed = 2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your current salary:");
+        salary = scanner.nextDouble();
+        System.out.println("Enter the duration in years at your current job:");
+        years = scanner.nextInt();
+
+        if (salary >= requiredSalary && years >= requiredYearsEmployed) {
+            System.out.println("Congrats! You qualify for the loan");
+        } else {
+            System.out.println("Sorry, you do not qualify for the loan");
+        }
+    }
+}
