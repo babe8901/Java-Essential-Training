70d93b4686df1050925ee220056d6db453907e92
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-31 00:44:29 +0530

    AddingTwoNumbers(switch expression) completed

---
+++ src/decision_structures/AddingTwoNumbers.java
@@ -0,0 +1,45 @@
+package decision_structures;
+
+import java.util.Scanner;
+
+public class AddingTwoNumbers {
+    public static void main(String[] args) {
+        float firstNumber, secondNumber, result;
+        char operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        firstNumber = scanner.nextFloat();
+        System.out.println("Enter the second number:");
+        secondNumber = scanner.nextFloat();
+        System.out.println("Enter the operation[+,-,*,/,%]:");
+        operation = scanner.next().charAt(0);
+
+        result = switch(operation) {
+            case '+' -> {
+                System.out.println("Adding...");
+                yield firstNumber + secondNumber;
+            }
+            case '-' -> {
+                System.out.println("Adding...");
+                yield firstNumber - secondNumber;
+            }
+            case '*' -> {
+                System.out.println("Adding...");
+                yield firstNumber * secondNumber;
+            }
+            case '/' -> {
+                System.out.println("Adding...");
+                yield firstNumber / secondNumber;
+            }
+            case '%' -> {
+                System.out.println("Adding...");
+                yield firstNumber % secondNumber;
+            }
+            default -> {
+                System.out.println("Invalid Operation");
+                yield 0;
+            }
+        };
+        System.out.println("The result is " + result);
+    }
+}
