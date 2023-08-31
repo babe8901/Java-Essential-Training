3068fb738c19e7eee1a75376ef11ca6fbabc00fe
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-31 17:17:02 +0530

    Rectangle(constructors) completed

--- src/objects/Rectangle.java
+++ src/objects/Rectangle.java
@@ -4,6 +4,16 @@ public class Rectangle {
     private double length;
     private double width;
 
+    public Rectangle() {
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double length, double width) {
+        setLength(length);
+        setWidth(width);
+    }
+
     public double calculatePerimeter() {
         return (2 * (length + width));
     }
