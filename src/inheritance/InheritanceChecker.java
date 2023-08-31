4bdfad4b0cebbc84cf1f1092ebddb57de42d5c0c
Shubham Yadav <luckyshubham16@gmail.com>
2023-08-31 23:57:29 +0530

    Rectangle, Square (overriding and overloading methods) completed

--- src/inheritance/InheritanceChecker.java
+++ src/inheritance/InheritanceChecker.java
@@ -2,7 +2,11 @@ package inheritance;
 
 public class InheritanceChecker {
     public static void main(String[] args) {
-//        Person person = new Person();
-        Employee employee = new Employee();
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square();
+
+        rectangle.print();
+        square.print();
+        square.print("what");
     }
 }
