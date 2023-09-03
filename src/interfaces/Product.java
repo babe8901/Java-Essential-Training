bd8a4c2ecd1bf97e43400ad978cdb14fe83ff47e
Shubham Yadav <luckyshubham16@gmail.com>
2023-09-03 14:01:15 +0530

    Library, Product(default and static methods)

--- src/interfaces/Product.java
+++ src/interfaces/Product.java
@@ -3,4 +3,13 @@ package interfaces;
 public interface Product {
     String getName();
     void setName(String name);
+
+    default double getPrice() {
+        return 50;
+    }
+    default void setPrice(double price) {}
+
+    static void intro() {
+        System.out.println("I am a Product Class");
+    }
 }
